e08b61469d182081ca08ff8a66e9174ca3a1cc4c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-27 20:46:39 +0700

    1234

--- src/pages/ss2.tsx
+++ src/pages/ss2.tsx
@@ -1,7 +1,11 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 type Props = {};
 
+// http://localhost:3000/ss2?token1=1234&token2=555
 export default function SS2({}: Props) {
-  return <div>SS2</div>;
+  const router = useRouter();
+
+  return <div>SS2 String Query: {JSON.stringify(router.query)}</div>;
 }
