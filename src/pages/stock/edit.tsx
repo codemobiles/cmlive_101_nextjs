6229c64f202fd121fac061accbb4d2f9e52695e8
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-27 20:26:55 +0700

    1234

---
+++ src/pages/stock/edit.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function StockEdit({}: Props) {
+  return <div>Stock Edit</div>;
+}
---
+++ src/pages/stock/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Stock({}: Props) {
+  return <div>Stock Root</div>;
+}
