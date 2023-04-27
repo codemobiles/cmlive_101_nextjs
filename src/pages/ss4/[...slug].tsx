63452dd559bb3156fc77f8ecfc2c893e176ba3ef
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-04-27 21:01:14 +0700

    1234

--- src/pages/ss4/[...slug].tsx
+++ src/pages/ss4/[...slug].tsx
@@ -3,20 +3,14 @@ import React from "react";
 
 type Props = {};
 
-// http://localhost:3000/ss3/token1
+// http://localhost:3000/ss4/1/2/3
 export default function SS4Slug({}: Props) {
   const router = useRouter();
   return (
     <div>
-      <div>
-        SS4Slug: {router.query.slug && JSON.stringify(router.query.slug[0])}
-      </div>
-      <div>
-        SS4Slug: {router.query.slug && JSON.stringify(router.query.slug[1])}
-      </div>
-      <div>
-        SS4Slug: {router.query.slug && JSON.stringify(router.query.slug[2])}
-      </div>
+      <div>SS4Slug: {JSON.stringify(router.query.slug?.[0])}</div>
+      <div>SS4Slug: {JSON.stringify(router.query.slug?.[1])}</div>
+      <div>SS4Slug: {JSON.stringify(router.query.slug?.[2])}</div>
     </div>
   );
 }
