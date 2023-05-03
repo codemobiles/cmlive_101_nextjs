6615ecf9152091e7bfab329c01b1702ddaff6c8a
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-05-03 20:06:19 +0700

    124

--- src/pages/ss6/[...song].tsx
+++ src/pages/ss6/[...song].tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
-type Props = {};
+type Props = {
+  id: string;
+};
 
-export default function Song({}: Props) {
-  return <div>Song</div>;
+export default function Song({ id }: Props) {
+  const [data, setData] = useState<any>({});
+
+  // Render at Client Side
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  async function loadData() {
+    const url =
+      "https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=songs";
+    const result = await fetch(url);
+    const data = await result.json();
+    const filter = data.youtubes.filter((e: any) => {
+      return e.id == id;
+    });
+
+    setData(filter[0]);
+  }
+
+  return (
+    <div>
+      <div>Id: {data.id}</div>
+      <div>Title: {data.title}</div>
+      <div>SubTitle: {data.subtitle}</div>
+    </div>
+  );
 }
+
+export const getServerSideProps = async (ctx: any) => {
+  console.log(ctx.query.song[0]);
+  return { props: { id: ctx.query.song[0] } };
+};
--- src/pages/ss6/index.tsx
+++ src/pages/ss6/index.tsx
@@ -9,12 +9,12 @@ type Props = {
 export default function SS6Index({ data1, youtubes }: Props) {
   return (
     <div>
-      <div>SS6Index</div>
+      <div>SS6Index (Server Side Rendering)</div>
       <div className="text-red-500">{JSON.stringify(data1)}</div>
       <ul>
         {youtubes.map(({ id, title }) => (
           <li key={id}>
-            <Link href="ss6/1">
+            <Link href={`ss6/${id}`}>
               <span className="text-yellow-400">{id}</span> | {title}
             </Link>
           </li>
@@ -24,6 +24,7 @@ export default function SS6Index({ data1, youtubes }: Props) {
   );
 }
 
+// Render Server side
 export const getServerSideProps = async () => {
   // https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=songs
   const url =
